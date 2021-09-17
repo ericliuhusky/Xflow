0a71efa51ac9d5751230457cbfc9cd5d73f63e6e
ericliuhusky <289471838@qq.com>
2021-09-17 18:35:09 +0800

    :fire: 删除工作流内核代码

--- Xflow/XflowApp.swift
+++ Xflow/XflowApp.swift
@@ -7,53 +7,28 @@
 
 import SwiftUI
 
-//@main
-//struct XflowApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//    }
-//}
-
 @main
-struct XflowApp {
-    static func main() throws {
-        Config.workspacePath = "/Users/lzh/Workspace"
-
-        let repo = SpecsRepo("http://git.babybus.co/Babybus-iOS/Specs/babybus-specs")
-        let podRepo = PodRepo("http://localhost:3000/gitea/NRDigitalVerifyView_Swift")
-
-        let newVersion = "21.9.16"
-
-        let flow = PodTagPushFlow(podRepo: podRepo)
-        try flow.modifySpec(version: newVersion)
-        
-        
-        // TODO: bookmark url
-        let data = UserDefaults.standard.data(forKey: "bookmark")
-        var isStale: Bool = false
-        if let data = data {
-            print(try? String(contentsOf: URL(resolvingBookmarkData: data, bookmarkDataIsStale: &isStale)))
-        }
-        
-//        try flow.gitPush(tag: newVersion)
-
-        //if !FileManager.default.fileExists(atPath: repo.path!) {
-        //    run("git clone \(repo.url)")
-        //}
-
-        //FileManager.default.changeCurrentDirectoryPath("/Users/lzh/workspace")
-        //try FileManager.default.createDirectory(at: URL(fileURLWithPath: "temp"), withIntermediateDirectories: false)
-
-        let panel = NSOpenPanel()
-        panel.canChooseDirectories = true
-        if panel.runModal() == .OK {
-            print(panel.url)
-            
-            let data = try panel.url?.bookmarkData()
-            UserDefaults.standard.set(data, forKey: "bookmark")
-            
+struct XflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
         }
     }
 }
+
+//// TODO: bookmark url
+//let data = UserDefaults.standard.data(forKey: "bookmark")
+//var isStale: Bool = false
+//if let data = data {
+//    print(try? String(contentsOf: URL(resolvingBookmarkData: data, bookmarkDataIsStale: &isStale)))
+//}
+//
+//let panel = NSOpenPanel()
+//panel.canChooseDirectories = true
+//if panel.runModal() == .OK {
+//    print(panel.url)
+//    
+//    let data = try panel.url?.bookmarkData()
+//    UserDefaults.standard.set(data, forKey: "bookmark")
+//    
+//}
