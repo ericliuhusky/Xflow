e93481b39b132bbde2b03b61054e242c0debd1b8
ericliuhusky <289471838@qq.com>
2021-09-18 18:15:46 +0800

    :construction: Podfile解析

--- Flow/Sources/Flow/main.swift
+++ Flow/Sources/Flow/main.swift
@@ -3,12 +3,52 @@ import AppKit
 
 
 Config.workspacePath = "/Users/lzh/Workspace"
-Config.specsRepoUrl = "http://localhost:3000/gitea/babybus-specs"
+//Config.specsRepoUrl = "http://localhost:3000/gitea/babybus-specs"
+//
+//let podRepo = PodRepo("http://localhost:3000/gitea/NRDigitalVerifyView_Swift")
+//
+//let newVersion = "23.23.23"
+//
+//let flow = PodTagPushFlow(podRepo: podRepo)
+//try flow.run(tag: newVersion)
 
-let podRepo = PodRepo("http://localhost:3000/gitea/NRDigitalVerifyView_Swift")
 
-let newVersion = "23.23.23"
+struct ProjectRepo {
+    let url: String
+    
+    init(_ url: String) {
+        self.url = url
+    }
+    
+    private var components: [String] {
+        if url.hasSuffix(".git") {
+            return url.dropLast(4).split(separator: "/").map { String($0) }
+        }
+        return url.split(separator: "/").map { String($0) }
+    }
+    
+    var name: String? {
+        components.last
+    }
+    
+    var path: String? {
+        guard
+            let name = name,
+            let workspacePath = Config.workspacePath
+        else { return nil }
+        return workspacePath + "/\(name)"
+    }
+    
+    var podfilePath: String? {
+        guard let path = path else { return nil }
+        return path + "/Podfile"
+    }
+}
 
-let flow = PodTagPushFlow(podRepo: podRepo)
-try flow.run(tag: newVersion)
+let repo = ProjectRepo("http://git.babybus.co/Platform-iOS/Business-Two/BabyRecommend/Baby_Recommend")
+let podfileContent = try! String(contentsOfFile: repo.podfilePath!)
+
+
+let podLines = podfileContent.regex.matches(pattern: #"\{\s*:names\s*=>\s*\['[a-zA-Z0-9_/]+'\],.*\}"#)
+print(podLines.count)
 
